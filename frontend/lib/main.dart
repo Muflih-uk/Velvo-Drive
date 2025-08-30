cd01acec2ab7dd08e7154948a46c8ffdf9f272b2
Muflih-uk <muhammedmuflih875@gmail.com>
2025-08-30 20:59:33 +0530

    Just Modify Home Page in Frontend

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -7,8 +7,6 @@ void main() {
   runApp(const MyApp());
 }
 
-// Thanks for using our template. You are using the free version of the template.
-// 🔗 Full template: https://theflutterway.gumroad.com/l/fluttershop
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,9 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shop Template by The Flutter Way',
+      title: 'VelvoDrive',
       theme: AppTheme.lightTheme(context),
-      // Dark theme is inclided in the Full template
       themeMode: ThemeMode.light,
       onGenerateRoute: router.generateRoute,
       initialRoute: onbordingScreenRoute,
