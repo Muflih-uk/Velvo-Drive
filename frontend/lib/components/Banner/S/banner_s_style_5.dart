104565a4a18af9a361c20369bd13bdc2bff54c57
Muflih-uk <muhammedmuflih875@gmail.com>
2025-08-31 09:52:29 +0530

    Black Friday Updated

--- frontend/lib/components/Banner/S/banner_s_style_5.dart
+++ frontend/lib/components/Banner/S/banner_s_style_5.dart
@@ -8,7 +8,7 @@ import 'banner_s.dart';
 class BannerSStyle5 extends StatelessWidget {
   const BannerSStyle5({
     super.key,
-    this.image = "https://i.imgur.com/wQ0sNHT.png",
+    this.image = "https://i.imgur.com/6EbSueO.jpeg",
     required this.title,
     required this.press,
     this.subtitle,
