44b896b2aa8c8bc45d0ff848ffdf4efde249fb8a
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-01 14:23:18 +0530

    User Model Created

--- backend/src/main/java/com/velvodrive/model/User.java
+++ backend/src/main/java/com/velvodrive/model/User.java
@@ -3,3 +3,15 @@ package com.velvodrive.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
+@Data
+@Table(name = "users")
+
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+}
