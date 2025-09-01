3f785b0a24e728c5dab7141d04778c1f65f07585
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-01 19:47:42 +0530

    Auth Service Created

---
+++ backend/src/main/java/com/velvodrive/service/AuthService.java
@@ -0,0 +1,56 @@
+package com.velvodrive.service;
+
+import com.velvodrive.dto.LoginRequest;
+import com.velvodrive.dto.RegisterRequest;
+import com.velvodrive.model.User;
+import com.velvodrive.repository.UserRepository;
+import com.velvodrive.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public void register(RegisterRequest registerRequest){
+        if(userRepository.findByUsername(registerRequest.getUsername().isPresent())){
+            throw new IllegalStateException("Username already exists!.");
+        }
+
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+
+        userRepository.save(user);
+    }
+
+    public String login(LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return jwtUtil.generateToken(authentication);
+    }
+}
