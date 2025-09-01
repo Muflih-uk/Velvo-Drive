c1042484ee4df315df55eb79c166b4de0be6ec0e
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-01 20:42:35 +0530

    Rental Model Created

--- backend/src/main/java/com/velvodrive/model/Rental.java
+++ backend/src/main/java/com/velvodrive/model/Rental.java
@@ -2,8 +2,21 @@ package com.velvodrive.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDate;
 
 @Entity
 @Data
 public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Vehicle vehicle;
+
+    @ManyToOne
+    private User renter;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
 }
