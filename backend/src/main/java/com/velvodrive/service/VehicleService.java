54ae307ff45d55b4f8803eb45aa940b184add0fe
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-01 21:52:40 +0530

    Vehicle Service Created

--- backend/src/main/java/com/velvodrive/service/VehicleService.java
+++ backend/src/main/java/com/velvodrive/service/VehicleService.java
@@ -1,1 +1,84 @@
 package com.velvodrive.service;
+
+import com.velvodrive.dto.VehicleDTO;
+import com.velvodrive.model.Rental;
+import com.velvodrive.model.User;
+import com.velvodrive.model.Vehicle;
+import com.velvodrive.repository.UserRepository;
+import jakarta.transaction.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class VehicleService {
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RentalRepository rentalRepository;
+
+    @Transactional
+    public Vehicle addVehicle(VehicleDTO vehicleDTO){
+        User owner = userRepository.findById(vehicleDTO.getOwnerId())
+                .orElseThrow( () ->new RuntimeException("Owner Not Found") );
+
+        Vehicle newVehicle = new Vehicle();
+        newVehicle.setOwner(owner);
+        newVehicle.setName(vehicleDTO.getName());
+        newVehicle.setDescription(vehicleDTO.getDescription());
+        newVehicle.setModel(vehicleDTO.getModel());
+        newVehicle.setPricePerDay(vehicleDTO.getPricePerDay());
+        newVehicle.setOwnerPhoneNumber(vehicleDTO.getOwnerPhoneNumber());
+        newVehicle.setMain_photo(vehicleDTO.getMain_photo());
+        newVehicle.setSecond_photo(vehicleDTO.getSecond_photo());
+        newVehicle.setThird_photo(vehicleDTO.getThird_photo());
+        newVehicle.setCreatedAt(vehicleDTO.getCreatedAt());
+        newVehicle.setAvailable(true);
+
+        return vehicleRepository.save(vehicle);
+    }
+
+    @Transactional
+    public Rental rentVehicle(Long vehicleId, Long renterId, int durationInDays) {
+        Vehicle vehicle = vehicleRepository.findById(vehicleId)
+                .orElseThrow(()->new RuntimeException("Vehicle not found"));
+
+        if (!vehicle.isAvailable()) {
+            throw new RuntimeException("Vehicle is not available for rent.");
+        }
+        User renter = userRepository.findById(renterId)
+                .orElseThrow(() -> new RuntimeException("Renter not found"));
+
+        vehicle.setAvailable(false);
+        vehicle.setRentalCount(vehicle.getRentalCount() + 1);
+        vehicleRepository.save(vehicle);
+
+        Rental rental = new Rental();
+        rental.setVehicle(vehicle);
+        rental.setRenter(renter);
+        rental.setStartDate(LocalDate.now());
+        rental.setEndDate(LocalDate.now().plusDays(durationInDays));
+
+        return rentalRepository.save(rental);
+
+    }
+
+    public List<Vehicle> getAvailableVehicles() {
+        return vehicleRepository.findAllByIsAvailableTrue();
+    }
+
+    public List<Vehicle> getPopularVehicles() {
+        return vehicleRepository.findAllByOrderByRentalCountDesc();
+    }
+
+    public List<Vehicle> getFlashSaleVehicles() {
+        return vehicleRepository.findAllByIsFlashSaleTrue();
+    }
+}
