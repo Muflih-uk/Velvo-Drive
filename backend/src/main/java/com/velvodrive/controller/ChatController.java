a5d57d42442e3501e92b2eaa99362e771de3a9fc
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-02 09:32:43 +0530

    Chat Controller Modified

--- backend/src/main/java/com/velvodrive/controller/ChatController.java
+++ backend/src/main/java/com/velvodrive/controller/ChatController.java
@@ -1,17 +1,28 @@
 package com.velvodrive.controller;
 
 import com.velvodrive.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/chat.sendMessage")
 
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
         return chatMessage;
+    }
+
+    @MessageMapping("/private.chat")
+    public void sendPrivateMessage(@Payload ChatMessage chatMessage) {
+        String destination = "/user/" + chatMessage.getRecipient() + "/queue/private";
+        simpMessagingTemplate.convertAndSend(destination, chatMessage);
     }
 }
