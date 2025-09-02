644592e20a7f5a8e19d2e638813414f4dcf7a7e1
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-02 09:11:31 +0530

    Create the main entry point

---
+++ backend/src/main/java/com/velvodrive/VelvoDrive.java
@@ -0,0 +1,11 @@
+package com.velvodrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelvoDrive {
+    public static void main(String[] args){
+        SpringApplication.run(VelvoDrive.class, args);
+    }
+}
