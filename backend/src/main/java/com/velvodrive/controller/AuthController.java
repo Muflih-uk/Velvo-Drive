31e74c3db678e7f169b1d0c70a2158d0b52abaaa
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-02 09:25:17 +0530

    Auth Controller added the Logout

--- backend/src/main/java/com/velvodrive/controller/AuthController.java
+++ backend/src/main/java/com/velvodrive/controller/AuthController.java
@@ -5,7 +5,10 @@ import com.velvodrive.dto.RegisterRequest;
 import com.velvodrive.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import com.velvodrive.service.TokenBlacklistService;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -13,6 +16,9 @@ public class AuthController{
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private TokenBlacklistService tokenBlacklistService;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
@@ -24,5 +30,15 @@ public class AuthController{
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
         String token = authService.login(loginRequest);
         return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request){
+        String authHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            tokenBlacklistService.addToBlacklist(token);
+        }
+        return ResponseEntity.ok("You've been successfully logged out.");
     }
 }
