37e2b68ea11924af70a15f2a4494dd0924629e31
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-02 08:21:22 +0530

    Featured Controller Created

---
+++ backend/src/main/java/com/velvodrive/controller/FeaturedController.java
@@ -0,0 +1,32 @@
+package com.velvodrive.controller;
+
+import com.velvodrive.model.Vehicle;
+import com.velvodrive.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/featured")
+public class FeaturedController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @GetMapping("/popular")
+    public ResponseEntity<List<Vehicle>> getPopularVehicle(){
+        return ResponseEntity.ok(vehicleService.getPopularVehicles());
+    }
+
+    @GetMapping("/flash-sales")
+    public ResponseEntity<List<Vehicle>> getFlashSaleVehicles(){
+        return ResponseEntity.ok(vehicleService.getFlashSaleVehicles());
+    }
+
+    @GetMapping("bestseller")
+    public ResponseEntity<List<Vehicle>> getBestSeller(){
+        return ResponseEntity.ok(vehicleService.getPopularVehicles());
+    }
+}
