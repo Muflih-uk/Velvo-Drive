79fe65c11092fb1c2005ed9fe0271c0b48652eb2
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-04 11:19:12 +0530

    AuthProvider Created

---
+++ frontend/lib/provider/auth_provider.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shop/services/auth_service.dart';
+
+class AuthProvider with ChangeNotifier{
+  final AuthService _authService = AuthService();
+
+  String? _token;
+  bool _isLoading = true;
+  String? _error;
+
+  String? get token => _token;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
+  bool get isAuthenticated => _token != null;
+
+  AuthProvider(){
+    _init();
+  }
+
+  Future<void> _init() async {
+    final prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('token');
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<bool> login(String email, String password) async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+    try {
+      final response = await _authService.login(email, password);
+      _token = response['token'];
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', _token!);
+      _isLoading = false;
+      notifyListeners();
+      return true;
+    } catch (e) {
+      _error = e.toString();
+      _isLoading = false;
+      notifyListeners();
+      return false;
+    }
+  }
+
+  Future<bool> register(String username, String email, String password) async {
+     _isLoading = true;
+    _error = null;
+    notifyListeners();
+     try {
+       await _authService.register(username, email, password);
+       return await login(email, password);
+     } catch(e) {
+       _error = e.toString();
+       _isLoading = false;
+       notifyListeners();
+       return false;
+     }
+  }
+
+  Future<void> logout() async {
+    _token = null;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+    notifyListeners();
+  }
+}
