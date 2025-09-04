f562c2c67acc9872f735d721b7bac38c1bb6d9f8
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-04 18:27:04 +0530

    Updated User Model Add Bookmark

--- backend/src/main/java/com/velvodrive/model/User.java
+++ backend/src/main/java/com/velvodrive/model/User.java
@@ -1,7 +1,11 @@
 package com.velvodrive.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -13,5 +17,15 @@ public class User{
     private long id;
     private String username;
     private String email;
+    @JsonIgnore
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_bookmarks",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "vehicle_id")
+    )
+    @JsonIgnore
+    private Set<Vehicle> bookmarkedVehicles = new HashSet<>();
 }
