960d065c40ecdf87b5b1a312fda8f7178a5a05ed
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-05 10:08:59 +0530

    API's are Created

--- frontend/lib/controllers/data_controller.dart
+++ frontend/lib/controllers/data_controller.dart
@@ -4,6 +4,7 @@ import 'package:shop/services/api_service.dart';
 class DataController {
   final ApiService _apiService = ApiService();
 
+  // Fetch full Vehicle 
   Future<List<dynamic>> fetchVehicles() async {
     try {
       final Response response = await _apiService.dio.get('vehicles/available');
@@ -14,4 +15,58 @@ class DataController {
     }
   }
 
+  // fetch User Details
+  Future<Map<String, dynamic>> fetchUserDetails() async {
+    try {
+      final Response response = await _apiService.dio.get('users/me');
+      return response.data;
+    } on DioException catch(e) {
+      print("Get Error in Fetch User Details: $e");
+      return {};
+    }
+  }
+
+  // fetch flash Sale
+  Future<List<dynamic>> fetchFlashSale() async {
+    try {
+      final Response response = await _apiService.dio.get('featured/flash-sales');
+      return response.data;
+    } on DioException catch(e) {
+      print("Get Error in Fetch Vehicle: $e");
+      return [];
+    }
+  }
+
+  // fetch popular Vehicle
+  Future<List<dynamic>> fetchPopularVehicle() async {
+    try {
+      final Response response = await _apiService.dio.get('featured/popular');
+      return response.data;
+    } on DioException catch(e) {
+      print("Get Error in Fetch Vehicle: $e");
+      return [];
+    }
+  }
+
+  // fetch flash Sale
+  Future<List<dynamic>> fetchUserVehicle() async {
+    try {
+      final Response response = await _apiService.dio.get('users/me/vehicles');
+      return response.data;
+    } on DioException catch(e) {
+      print("Get Error in Fetch Vehicle: $e");
+      return [];
+    }
+  }
+
+  // Show the Bookmark
+  Future<List<dynamic>> fetchBookmarkedVehicle() async {
+    try {
+      final Response response = await _apiService.dio.get('users/me/bookmarks');
+      return response.data;
+    } on DioException catch(e) {
+      print("Get Error in Fetch Vehicle: $e");
+      return [];
+    }
+  }
 }
